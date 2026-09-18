1799abac0a1c2e7a3b86331604e149500683e9d0
anttiluode <67085641+anttiluode@users.noreply.github.com>
2026-09-18 08:05:58 +0300

    Export virtual organoid API

--- src/frequency_modal_cell/__init__.py
+++ src/frequency_modal_cell/__init__.py
@@ -2,6 +2,14 @@
 
 from .axon import AxonArbor, AxonSegment, ephaptic_contacts, grow_branching_arbor, terminal_arrival_times
 from .core import ResonantActiveCable, ReducedModel, carrier_packet, fit_reduced_model, rollout_reduced
+from .virtual_organoid import (
+    LearnedCodebook,
+    LinearPortReservoir,
+    StimulusAction,
+    VirtualOrganoid,
+    evaluate_codebook,
+    learn_stimulation_codebook,
+)
 
 __all__ = [
     "AxonArbor",
@@ -14,4 +22,10 @@ __all__ = [
     "carrier_packet",
     "fit_reduced_model",
     "rollout_reduced",
+    "LearnedCodebook",
+    "LinearPortReservoir",
+    "StimulusAction",
+    "VirtualOrganoid",
+    "evaluate_codebook",
+    "learn_stimulation_codebook",
 ]
